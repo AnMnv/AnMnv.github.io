afd3a31c5a2fb2bd1d3c6686d63cd598baf3ae99
anmnv <antonmn1412@gmail.com>
2025-01-06 23:48:53 +0100

    new

--- assets/js/all.js
+++ assets/js/all.js
@@ -119,7 +119,7 @@ window.addEventListener('DOMContentLoaded', async function() {
     el.innerHTML = `
     <div style="font-family: Segoe UI Emoji; font-weight: 600; padding: 16px; font-size: 18px; line-height: 1.5; color: #24292e;">
       <div style="display: flex; align-items: center;">
-        <svg style="fill:rgb(21, 22, 23); margin-right: 8px;" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"></path></svg>
+        <svg style="fill:rgb(77, 92, 106); margin-right: 8px;" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"></path></svg>
         <span style="font-weight: 600; color: #0366d6;">
           <a style="text-decoration: none; color: inherit;" href="${data.html_url}">${data.name}</a>
         </span>
