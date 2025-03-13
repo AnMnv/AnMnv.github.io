3c600be9e703cbe0ab18f74838ce06669957c90f
anmnv <antonmn1412@gmail.com>
2025-03-13 23:02:18 +0100

    v_7_1

--- assets/js/all.js
+++ assets/js/all.js
@@ -25,8 +25,9 @@ document.addEventListener("DOMContentLoaded", () => {
       { layer: "9", yPercent: 10 },
       { layer: "888", yPercent: 20 },
       { layer: "555", yPercent: 10 },
-      { layer: "999", yPercent: 70 },
-      { layer: "1000", yPercent: 30 }
+      { layer: "999", yPercent: 40 },
+      { layer: "1000", yPercent: 10 },
+      { layer: "1001", yPercent: 10 }
     ];
     layers.forEach((layerObj, idx) => {
       tl.to(
@@ -190,9 +191,9 @@ document.addEventListener("DOMContentLoaded", function () {
       { id: 'startButton', audioSrc: 'assets/audio/pain.mp3', gifSrc: 'https://i.imgur.com/4RaR7zg.gif', parallaxLayer: '444', text: `\nFeel Pain. Accept Pain. And Know Pain. \n\n⏸Those Who Do Not Know Pain, Will Never Understand True Peace.\n\n⏸And Now...`, delays: { text: 0, audio: 0, gif: 12200, gifDuration: 5800 } },
       { id: 'startButton2', audioSrc: 'assets/audio/sasuke_audio.mp3', gifSrc: 'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHhzeGtxa25nMWQ3cmpwZjdkdHphOHp3cXM0NWpmMmNmdXR1OXQ0MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/X4s4RRkT5F5pS/giphy.gif', parallaxLayer: '555', text: `\n\n\n \t    These eyes, see darkness clearly ...`, delays: { text: 0, audio: 0, gif: 1500, gifDuration: 1900 } },
       { id: 'startButton3', audioSrc: 'assets/audio/madara.mp3', gifSrc: 'https://i.imgur.com/RvjbMz2.gif', parallaxLayer: '888', text: `\n\n\n \t   I am ... ⏸ the ghost of the Uchiha ...`, delays: { text: 0, audio: 0, gif: 4900, gifDuration: 6800 } },
-      { id: 'startButton4', audioSrc: 'assets/audio/konan_obito.mp3', gifSrc: 'https://i.imgur.com/4JbSMWM.gif', parallaxLayer: '999', text: `\n\n\n \t\t   光のない世界に花は枯れる `, delays: { text: 0, audio: 0, gif: 100, gifDuration: 4900 } },
-      { id: 'startButton5', audioSrc: 'assets/audio/toji.mp3', gifSrc: 'https://i.imgur.com/e2PkD86.mp4', parallaxLayer: '1000', text: `\n\n\n \tПохоже к нунчакам прилипла какая-то фигня ... `, delays: { text: 12000, audio: 100, gif: 0, gifDuration: 12900 } },
-      { id: 'startButton6', audioSrc: 'assets/audio/itachi.mp3', gifSrc: 'https://i.imgur.com/uAA90ks.mp4', parallaxLayer: '1000', text: `\n\n\n \t\t    Sono Sharingan ... `, delays: { text: 2000, audio: 1100, gif: 1000, gifDuration: 6100 } }
+      { id: 'startButton4', audioSrc: 'assets/audio/konan_obito.mp3', gifSrc: 'https://i.imgur.com/4JbSMWM.gif', parallaxLayer: '999', text: `\n\n\n \t\t   光のない世界に花は枯れる `, delays: { text: 0, audio: 0, gif: 100, gifDuration: 4500 } },
+      { id: 'startButton5', audioSrc: 'assets/audio/toji.mp3', gifSrc: 'https://i.imgur.com/e2PkD86.mp4', parallaxLayer: '1001', text: `\n\n\n \tПохоже к нунчакам прилипла какая-то фигня ... `, delays: { text: 12000, audio: 100, gif: 0, gifDuration: 12900 } },
+      { id: 'startButton6', audioSrc: 'assets/audio/itachi.mp3', gifSrc: 'https://i.imgur.com/uAA90ks.mp4', parallaxLayer: '1000', text: `\n\n\n \t\t    Sono Sharingan ... `, delays: { text: 2000, audio: 100, gif: 100, gifDuration: 6100 } }
   ];
 
   function setupButton({ id, audioSrc, gifSrc, parallaxLayer, text, delays }) {
